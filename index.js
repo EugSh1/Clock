b846e169fb4dde416f389898545eae11ae374f79
EugSh1 <eugshlepanov@gmail.com>
2023-08-16 17:25:45 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//----------Date block----------
+setInterval(function(){
+    let today = new Date();
+    let hour = today.getHours();
+    let minutes = today.getMinutes();
+    document.getElementById("timetext").innerHTML = `${hour}:${String(minutes).padStart(2, '0')}`;
+}, 500);
+//------------------------------
