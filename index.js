11fbd856055c29bc3f99d50c4223a5ca798c41c2
EugSh1 <eugshlepanov@gmail.com>
2023-08-19 20:00:34 +0300

    Now time is const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 setInterval(function(){
-    let today = new Date();
-    let hour = today.getHours();
-    let minutes = today.getMinutes();
+    const today = new Date();
+    const hour = today.getHours();
+    const minutes = today.getMinutes();
 
     document.getElementById("timetext").innerHTML = `${hour}:${String(minutes).padStart(2, '0')}`;
 }, 500);
