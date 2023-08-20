05b58fefd41fab8d055f52f443b6c4462c7741a5
EugSh1 <eugshlepanov@gmail.com>
2023-08-20 18:46:00 +0300

    Added change of design themes

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ setInterval(function(){
 
     document.getElementById("timetext").innerHTML = `${hour}:${String(minutes).padStart(2, '0')}`;
 }, 500);
+
+
+const darkRadioButton = document.getElementById("greenRadioButton");
+const lightRadioButton = document.getElementById("blueRadioButton");
+
+const div = document.getElementById("divclock");
+const clock = document.getElementById("timetext");
+
+darkRadioButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#1B1C18";
+    div.style.backgroundColor = "#22261D";
+    clock.style.color = "#E4E3DD";
+});
+
+lightRadioButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#1A1C1E";
+    div.style.backgroundColor = "#212529";
+    clock.style.color = "#E4E3DD";
+});
