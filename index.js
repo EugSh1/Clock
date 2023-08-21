14d12d2823af77d9d15b3bd7021a7eb667718c4b
EugSh1 <eugshlepanov@gmail.com>
2023-08-21 10:11:23 +0300

    Added 2 color themes

--- index.js
+++ index.js
@@ -7,20 +7,34 @@ setInterval(function(){
 }, 500);
 
 
-const darkRadioButton = document.getElementById("greenRadioButton");
-const lightRadioButton = document.getElementById("blueRadioButton");
+const greenRadioButton = document.getElementById("greenRadioButton");
+const blueRadioButton = document.getElementById("blueRadioButton");
+const redRadioButton = document.getElementById("redRadioButton");
+const yellowRadioButton = document.getElementById("yellowRadioButton");
 
 const div = document.getElementById("divclock");
 const clock = document.getElementById("timetext");
 
-darkRadioButton.addEventListener("click", () => {
+greenRadioButton.addEventListener("click", () => {
     document.body.style.backgroundColor = "#1B1C18";
     div.style.backgroundColor = "#22261D";
     clock.style.color = "#E4E3DD";
 });
 
-lightRadioButton.addEventListener("click", () => {
+blueRadioButton.addEventListener("click", () => {
     document.body.style.backgroundColor = "#1A1C1E";
     div.style.backgroundColor = "#212529";
+    clock.style.color = "#E4E3DD";
+});
+
+redRadioButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#201A19";
+    div.style.backgroundColor = "#2A2220";
+    clock.style.color = "#E4E3DD";
+});
+
+yellowRadioButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#1C1C19";
+    div.style.backgroundColor = "#25251F";
     clock.style.color = "#E4E3DD";
 });
